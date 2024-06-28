c5cbb64dbf9574ab3c6e1191001774177a93166d
Shida <marcellinhonkai@gmail.com>
2024-06-27 22:03:56 -0300

    firstCommit

--- src/exNotaAluno.java
+++ src/exNotaAluno.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class exNotaAluno {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exNotaAluno {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        double nota1, nota2, notaFinal;
+        nota1 = input.nextDouble();
+        nota2 = input.nextDouble();
+        input.close();
+
+        notaFinal = nota1 + nota2;
+        System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
+
+        if (notaFinal < 60.00) {
+            System.out.println("REPROVADO!");
+        } else {
+            System.out.println("APROVADO!");
+        }
+
+    }
 }
